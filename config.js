fa614d6497496c7af030845285af102a281cc68a
danielmentxaka <danielmentxaka@gmail.com>
2026-09-18 16:25:56 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// Feo Puerto Live - GitHub Pages + Supabase.
+window.FEO_SUPABASE_URL = "https://wodyxgpwaxpyprmyiwtm.supabase.co";
+window.FEO_SUPABASE_ANON_KEY = "sb_publishable_m6BjJc2rxt16S7nOH8sw3Q_DtNW_s0L";
